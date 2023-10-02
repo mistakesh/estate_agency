f6db109b4c2b9136d7d5698c7e08079a4a080984
mistakesh <ekenejustice6@gmail.com>
2023-10-02 23:01:29 +0100

    first commit

--- estate/settings.py
+++ estate/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j@#g9kzp@96y*na3p6+t$5&(=mrs#z&$0qri^)7nfc1@(h^fc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'estateangency.onrender.com' ]
 
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media Folder
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
