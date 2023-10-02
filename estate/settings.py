99c0b46688c426eb5e99e3dd313dbe47da0fc5f6
mistakesh <ekenejustice6@gmail.com>
2023-10-03 00:20:42 +0100

    first commit

--- estate/settings.py
+++ estate/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "estate" /"static"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Media Folder
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
