5289b7e05c745222207f079ac885f7c7501fdd74
mistakesh <ekenejustice6@gmail.com>
2023-10-02 13:10:01 +0100

    first commit

--- estate/settings.py
+++ estate/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR/'assets'
 STATICFIILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # media folder
