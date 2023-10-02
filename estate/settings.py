49bc4d5bacfd7f47d8f781c80fdf530b4bbe2d8d
mistakesh <ekenejustice6@gmail.com>
2023-10-03 00:18:07 +0100

    first commit

--- estate/settings.py
+++ estate/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j@#g9kzp@96y*na3p6+t$5&(=mrs#z&$0qri^)7nfc1@(h^fc('
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-j@#g9kzp@96y*na3p6+t$5&(=mrs#z&$0qri^)7nfc1@(h^fc(') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
