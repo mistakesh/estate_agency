3a727cba30038bc3dbd6ba44f9b164a9d81060be
mistakesh <ekenejustice6@gmail.com>
2023-10-07 14:47:08 +0100

    last Commit

--- estate/settings.py
+++ estate/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-j@#g9kzp@96y*na3p6+t$
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(",")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*')
 
 # Application definition
 
